3202859f58c86bbafc1931636cf8e726fa240eda
donhuvy <74403808+vyolbius@users.noreply.github.com>
2021-09-12 22:59:59 +0700

    create table

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""djangoProject URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib import admin
-from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
 ]
